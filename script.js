094a7974be3fd5e3c53c5af83155ee7a8a611a7f
Bowie <miguel.waldeck@hotmail.com>
2024-05-19 19:18:32 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const menu = document.getElementById("menu")
+const cartBtn = document.getElementById("cart-btn")
+const cartModal = document.getElementById("cart-modal")
+const cartItemsContainer = document.getElementById("card-items")
+const cartTotal = document.getElementById("cart-total")
+const checkOut = document.getElementById("checkout-btn")
+const closeModalBtn = document.getElementById("close-modal-btn")
+const cartCounter = document.getElementById("cart-count")
+const addressInput =document.getElementById("address")
+const addressWarn =document.getElementById("address-warn")
+
+
