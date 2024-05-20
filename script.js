cee0d05d19071a966663fbefe649c356d242f26d
Bowie <miguel.waldeck@hotmail.com>
2024-05-20 13:25:44 -0300

    Continuação JavaScript

--- script.js
+++ script.js
@@ -9,4 +9,43 @@ const cartCounter = document.getElementById("cart-count")
 const addressInput =document.getElementById("address")
 const addressWarn =document.getElementById("address-warn")
 
+let cart = [];
 
+// ABRIR MODAL DO CARRINHO
+cartBtn.addEventListener("click", ()=>{
+cartModal.style.display = "flex"
+})
+
+// FECHAR O MODAL CLICANDO FORA
+cartModal.addEventListener("click", (event)=>{
+    if(event.target === cartModal){
+        cartModal.style.display = "none"
+    }
+})
+
+closeModalBtn.addEventListener("click", ()=>{
+    cartModal.style.display = "none"
+})
+
+menu.addEventListener("click", (event)=>{
+
+
+let parentButton = event.target.closest(".add-to-cart-btn")
+    if(parentButton){
+        const name = parentButton.getAttribute("data-name")
+        const price = parseFloat(parentButton.getAttribute("data-price"))
+        addToCart(name, price)
+    }
+})
+
+//Função para adicionar no carrinho
+
+
+ let addToCart = (name, price)=>{
+    
+    cart.push({
+        name,
+        price,
+        quantity: 1
+    })
+ }
